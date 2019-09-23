c2baabe90f4443e7fc6bf907b4f5f50e9016f688
mansii-agarwal <55138466+mansii-agarwal@users.noreply.github.com>
2019-09-23 16:02:30 +0530

    New classes

---
+++ pecunianew/src/main/java/com/capgemini/pecunia/service/PassbookMaintenanceServiceImpl.java
@@ -0,0 +1,24 @@
+package com.capgemini.pecunia.service;
+
+import java.util.ArrayList;
+
+import com.capgemini.pecunia.dto.Transaction;
+
+
+
+public class PassbookMaintenanceServiceImpl implements PassbookMaintenanceService
+{
+
+	@Override
+	public ArrayList<Transaction> updatePassbook(String accountId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/*public boolean updatePassbook(String accountId)
+	{
+		ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
+		passbookDAO= new PassbookMaintenanceDAOImpl();
+		updatedList=passbookDAO.updatePassbook(String accountId);
+		
+	}*/
+}
