23b56ed36df811b825047c24d8689ce98bfa780a
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-09-23 09:32:05 +0530

    Update Account Service Layer

--- pecunianew/src/main/java/com/capgemini/pecunia/service/AccountManagementServiceImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/service/AccountManagementServiceImpl.java
@@ -1,10 +1,14 @@
 package com.capgemini.pecunia.service;
 
+import com.capgemini.pecunia.dao.AccountManagementDAO;
+import com.capgemini.pecunia.dao.AccountManagementDAOImpl;
 import com.capgemini.pecunia.dto.Account;
 import com.capgemini.pecunia.dto.Address;
 import com.capgemini.pecunia.dto.Customer;
 
 public class AccountManagementServiceImpl implements AccountManagementService{
+	
+	AccountManagementDAO accountDAO;
 
 	@Override
 	public boolean deleteAccount(String accountId) {
@@ -14,20 +18,40 @@ public class AccountManagementServiceImpl implements AccountManagementService{
 
 	@Override
 	public boolean updateCustomerName(String accountId, Customer cust) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		/*
+		 * Function takes the accountID and the customer object(which contains the updated name)
+		 * as arguments, updates the database, and returns a boolean value
+		 */
+		boolean updated = false;
+		accountDAO = new AccountManagementDAOImpl();
+		updated = accountDAO.updateCustomerName(accountId, cust);
+		return updated;
+		
 	}
 
 	@Override
 	public boolean updateCustomerContact(String accountId, Customer cust) {
-		// TODO Auto-generated method stub
-		return false;
+		/*
+		 * Function takes the accountID and the customer object(which contains the updated name)
+		 * as arguments, updates the database, and returns a boolean value
+		 */
+		boolean updated = false;
+		accountDAO = new AccountManagementDAOImpl();
+		updated = accountDAO.updateCustomerContact(accountId, cust);
+		return updated;
 	}
 
 	@Override
 	public boolean updateCustomerAddress(String accountId, Address add) {
-		// TODO Auto-generated method stub
-		return false;
+		/*
+		 * Function takes the accountID and the customer object(which contains the updated name)
+		 * as arguments, updates the database, and returns a boolean value
+		 */
+		boolean updated = false;
+		accountDAO = new AccountManagementDAOImpl();
+		updated = accountDAO.updateCustomerAddress(accountId, add);
+		return updated;
 	}
 
 	@Override
