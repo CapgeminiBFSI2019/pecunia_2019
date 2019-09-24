549f6f4bb52cf405d341f0a5bcd8684d98d7a825
risrai <55137949+risrai@users.noreply.github.com>
2019-09-24 14:08:50 +0530

    risrai pushes

--- pecunianew/src/main/java/com/capgemini/pecunia/exception/ErrorConstants.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/exception/ErrorConstants.java
@@ -3,22 +3,17 @@
  */
 package com.capgemini.pecunia.exception;
 
-/**
- * @author mansagar
- *
- */
+
 public class ErrorConstants {
 	
 
 	public static final String techError= "Technical problem occured. Refer log";
-	//TODO
-
-	public static final String techError= "Tehnical problem occured. Refer log";
 	
 	public static final String updatePassbookError= "Passbook not updated. There is some technical error.";
 	
 	public static final String dbConnectionError= "Error in closing connection";
 	
+	public static final String LoanAddError= "Error in performing operation.";
 
 
 }
