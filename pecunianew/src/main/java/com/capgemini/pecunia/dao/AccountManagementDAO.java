86d7dad19db7aa7081917a22cd32f5b9e70cc69a
Rohan Patil <rohanpatil365@gmail.com>
2019-09-24 11:59:47 +0530

    too many changes

--- pecunianew/src/main/java/com/capgemini/pecunia/dao/AccountManagementDAO.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/dao/AccountManagementDAO.java
@@ -3,6 +3,7 @@ package com.capgemini.pecunia.dao;
 import com.capgemini.pecunia.dto.Account;
 import com.capgemini.pecunia.dto.Address;
 import com.capgemini.pecunia.dto.Customer;
+import com.capgemini.pecunia.exception.MyException;
 
 public interface AccountManagementDAO {
 	public boolean deleteAccount(String accountId);
@@ -10,6 +11,6 @@ public interface AccountManagementDAO {
 	public boolean updateCustomerContact(String accountId, Customer cust);
 	public boolean updateCustomerAddress(String accountId, Address add);
 	public String addAccount(Customer cust, Address add, Account acc);
-	public String calculateAccountId(String id);
+	public String calculateAccountId(String id) throws MyException;
 
 }
