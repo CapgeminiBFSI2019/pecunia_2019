7f7fccc7aedd56d914bfedcb337b18bbab345dd0
im4gingchords <55138357+im4gingchords@users.noreply.github.com>
2019-09-24 18:22:52 +0530

    updated

--- pecunianew/src/main/java/com/capgemini/pecunia/pl/Demo.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/pl/Demo.java
@@ -1,9 +1,42 @@
 package com.capgemini.pecunia.pl;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.capgemini.pecunia.dao.LoanDisbursalDAO;
+import com.capgemini.pecunia.dao.LoanDisbursalDAOImpl;
+import com.capgemini.pecunia.dao.TransactionDAO;
+import com.capgemini.pecunia.dao.TransactionDAOImpl;
+import com.capgemini.pecunia.dto.Account;
+import com.capgemini.pecunia.dto.Loan;
+import com.capgemini.pecunia.exception.MyException;
+import com.capgemini.pecunia.exception.TransactionException;
 
 public class Demo {
 	public static void main(String[] args) {
+		TransactionDAO  transactionDAO = new TransactionDAOImpl();
+		Account account = new Account();
+		account.setId("100101000001");
+		try {
+			double bal = transactionDAO.getbalance(account);
+			System.out.println(bal);
+		} catch (MyException | TransactionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-	}
+		LoanDisbursalDAO loanDisbursalDAO = new LoanDisbursalDAOImpl();
+		List<Loan> l = new ArrayList<Loan>();
+		try {
+			l = loanDisbursalDAO.retrieveLoanList();
+		    loanDisbursalDAO.releaseLoanSheet(l);
+			System.out.println(l);
+		} catch (IOException | MyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+}
 }
