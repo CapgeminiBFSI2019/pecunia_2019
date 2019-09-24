ffa3234b1170716783584568709cdfb3a87cd326
Rohan Patil <rohanpatil365@gmail.com>
2019-09-24 15:47:57 +0530

    Update ErrorConstants.java

--- pecunianew/src/main/java/com/capgemini/pecunia/exception/ErrorConstants.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/exception/ErrorConstants.java
@@ -3,30 +3,19 @@
  */
 package com.capgemini.pecunia.exception;
 
-
 public class ErrorConstants {
-	
 
+	public static final String techError = "Technical problem occured. Refer log";
 
-	
+	public static final String updatePassbookError = "Passbook not updated. There is some technical error.";
 
-	public static final String techError= "Technical problem occured. Refer log";
+	public static final String dbConnectionError = "Error in closing connection.";
 
+	public static final String updateAccountError = "Update unsuccessful.";
+	public static final String deletAccountError = "Deletion unsuccessful.";
+	public static final String accountCreationError = "Account not Created.";
+	public static final String LoginError = "Either Password or Email is incorrect";
 
-	
-
-
-
-	public static final String updatePassbookError= "Passbook not updated. There is some technical error.";
-	
-	public static final String dbConnectionError= "Error in closing connection.";
-	
-
-public static final String updateAccountError = "Update unsuccessful.";
-public static final String deletAccountError = "Deletion unsuccessful.";
-public static final String accountCreationError = "Account not Created.";
-public static final String LoginError = "Either Password or Email is incorrect";
-
-	public static final String LoanAddError= "Error in performing operation.";
+	public static final String LoanAddError = "Error in performing operation.";
 
 }
