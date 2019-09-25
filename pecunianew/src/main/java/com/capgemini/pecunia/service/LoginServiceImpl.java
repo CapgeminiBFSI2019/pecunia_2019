f4a0083cbe17170d41e4ad031cdf6aea5f3eb839
saurabh5881 <55138472+saurabh5881@users.noreply.github.com>
2019-09-25 14:08:20 +0530

    logger defined for login
    gsgt

--- pecunianew/src/main/java/com/capgemini/pecunia/service/LoginServiceImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/service/LoginServiceImpl.java
@@ -3,6 +3,9 @@ package com.capgemini.pecunia.service;
 
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.capgemini.pecunia.dao.LoginDAO;
 import com.capgemini.pecunia.dao.LoginDAOImpl;
 import com.capgemini.pecunia.dto.Login;
@@ -12,13 +15,17 @@ import com.capgemini.pecunia.exception.MyException;
 import com.capgemini.pecunia.util.Utility;
 
 public class LoginServiceImpl implements LoginService{
-
+	Logger logger = Logger.getRootLogger();
+	public LoginServiceImpl() {
+		PropertyConfigurator.configure("resources//log4j.properties");
+	}
 	LoginDAO loginDAO = new LoginDAOImpl();
 	@Override
 	public boolean validateEmail(Login log) throws MyException, LoginException {
 		boolean flag=false;
 		String salt = loginDAO.validateEmail(log);
 		if(salt==null) {
+			logger.error("validation failed ");
 			throw new LoginException(ErrorConstants.LOGIN_ERROR);
 		}
 		else {
@@ -26,6 +33,7 @@ public class LoginServiceImpl implements LoginService{
 			try {
 				arr = Utility.getSHA(log.getPassword() + "" + salt);
 			} catch (NoSuchAlgorithmException e) {
+				logger.error("validation failed ");
 				throw new LoginException(ErrorConstants.LOGIN_ERROR);
 			}
 			String hashPassword = Utility.toHexString(arr);
@@ -33,6 +41,7 @@ public class LoginServiceImpl implements LoginService{
 			try {
 				flag = loginDAO.validatePassword(logNew);
 			} catch (LoginException e) {
+				logger.error("Validation failed ");
 				throw new LoginException(ErrorConstants.LOGIN_ERROR);
 			}
 		}
