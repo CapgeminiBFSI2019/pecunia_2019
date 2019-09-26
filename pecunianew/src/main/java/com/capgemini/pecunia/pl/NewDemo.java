bafef21a0900c6cb03fd9a7aaee9a6e8c7cc36a5
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-09-26 14:20:25 +0530

    hua hua
    hello, updated!

--- pecunianew/src/main/java/com/capgemini/pecunia/pl/NewDemo.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/pl/NewDemo.java
@@ -15,24 +15,29 @@ public class NewDemo {
 
 	public static void main(String[] args) throws MyException, AccountException {
 		
-		LocalDate date = LocalDate.parse("1996-05-01");
-		
-//		Account acc = new Account("","","1003","Current","Active",0.00,0.00,null);
-//		Customer cust = new Customer("","Nikhil","","123458989876","18264578AQ","9956472628","F", date);
-//		Address add = new Address("","123-Manipal Heights","Whitefield","Bangalore","Karnataka","India","560032");
+//		LocalDate date = LocalDate.parse("1996-05-01");
 //		
-		//Account acc = new Account("100202000002","","","","",0.00,0.00,null);
-		AccountManagementService ams = new AccountManagementServiceImpl();
-//		String create = ams.addAccount(cust, add, acc);
-//		System.out.println("Account created. Acc ID is: "+ create);
+////		Account acc = new Account("","","1003","Current","Active",0.00,0.00,null);
+////		Customer cust = new Customer("","Nikhil","","123458989876","18264578AQ","9956472628","F", date);
+////		Address add = new Address("","123-Manipal Heights","Whitefield","Bangalore","Karnataka","India","560032");
+////		
+//		//Account acc = new Account("100202000002","","","","",0.00,0.00,null);
+			AccountManagementService ams = new AccountManagementServiceImpl();
+////		String create = ams.addAccount(cust, add, acc);
+////		System.out.println("Account created. Acc ID is: "+ create);
+//		
+//		Account acc = new Account("100202000001","","","","",0.00,0.00,null);
+//		//Customer cust = new Customer("","","","","","8879771134","", date);
+//		//Address add = new Address("","123-Sathya Sai Heights","Whitefield","Bangalore","Karnataka","India","560066");
+//		//Boolean vali = ams.updateCustomerAddress(acc, add);
+//		
+//		Boolean vali =ams.deleteAccount(acc);
+//		System.out.println(vali);
 		
-		Account acc = new Account("100202000001","","","","",0.00,0.00,null);
-		//Customer cust = new Customer("","","","","","8879771134","", date);
-		//Address add = new Address("","123-Sathya Sai Heights","Whitefield","Bangalore","Karnataka","India","560066");
-		//Boolean vali = ams.updateCustomerAddress(acc, add);
-		
-		Boolean vali =ams.deleteAccount(acc);
-		System.out.println(vali);
+		Account acc1=new Account();
+		acc1.setId("10FDGFHJI1");
+		boolean val = ams.validateAccountId(acc1);
+		System.out.println(val);
 //		
 
 	}
