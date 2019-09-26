9520057fca3b4f053a9ee40f69f9b33ba6c7bf30
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-09-26 09:07:23 +0530

    something

--- pecunianew/src/main/java/com/capgemini/pecunia/pl/NewDemo.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/pl/NewDemo.java
@@ -3,6 +3,7 @@ package com.capgemini.pecunia.pl;
 import java.util.Date;
 
 import com.capgemini.pecunia.dto.Account;
+import com.capgemini.pecunia.dto.Address;
 import com.capgemini.pecunia.dto.Customer;
 import com.capgemini.pecunia.exception.AccountException;
 import com.capgemini.pecunia.exception.MyException;
@@ -13,11 +14,15 @@ public class NewDemo {
 
 	public static void main(String[] args) throws MyException, AccountException {
 		
-		Date date = new Date();
-		Account acc = new Account("100101000001","","","","",0.00,0.00,null);
-		Customer cust = new Customer("","Darshana Mehra","","","","","",null);
+		@SuppressWarnings("deprecation")
+		Date date = new Date("1996-05-01");
+		
+		Account acc = new Account("","","1001","Current","Active",0.00,0.00,null);
+		Customer cust = new Customer("","Aditi Singh","","123456789876","17264578AQ","9856472628","F",(java.sql.Date) date);
+		Address add = new Address("","123-Manipal Heights","Whitefield","Bangalore","Karnataka","India","560032");
+		
 		AccountManagementService ams = new AccountManagementServiceImpl();
-		boolean vali = ams.updateCustomerName(acc, cust);
+		String vali = ams.addAccount(cust, add, acc);
 		System.out.println(vali);
 	}
 
