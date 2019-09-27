6274fa3acc5be507769ad14c7fc44ad9e77d1662
Rishav Dev <55138547+DevRishav@users.noreply.github.com>
2019-09-27 15:54:35 +0530

    risavdev commits passbook edits

--- pecunianew/src/main/java/com/capgemini/pecunia/dao/PassbookMaintenanceDAOImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/dao/PassbookMaintenanceDAOImpl.java
@@ -1,226 +1,91 @@
-package com.capgemini.pecunia.dao;
+public static void main(String[] args) throws MyException, PassbookException  {
+     
+		PassbookMaintenanceService PassbookService = new PassbookMaintenanceServiceImpl();
+		List<Transaction> updatePassbook = new ArrayList<Transaction>();
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter accountId:");
+		String accountId = obj.nextLine();
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-
-import com.capgemini.pecunia.dto.Transaction;
-import com.capgemini.pecunia.exception.ErrorConstants;
-import com.capgemini.pecunia.exception.MyException;
-import com.capgemini.pecunia.exception.PassbookException;
-import com.capgemini.pecunia.util.DBConnection;
-
-public class PassbookMaintenanceDAOImpl implements PassbookMaintenanceDAO {
-
-	Logger logger = Logger.getRootLogger();
-
-	public PassbookMaintenanceDAOImpl() {
-		PropertyConfigurator.configure("resources//log4j.properties");
-
-	}
-
-	/*******************************************************************************************************
-	 * - Function Name : updatePassbook(String accountId) 
-	 * - Input Parameters : String accountId
-	 * - Return Type : List 
-	 * - Throws : PassbookException, MyException 
-	 * - Author : Mansi Agarwal
-	 * - Creation Date : 24/09/2019 
-	 * - Description : Stores the transaction details in the list and returns it to service layer
-	 ********************************************************************************************************/
+	try {
+		updatePassbook = PassbookService.updatePassbook(accountId);
 	
-	@Override
-	public List<Transaction> updatePassbook(String accountId) throws PassbookException, MyException {
 
-		Connection connection = DBConnection.getInstance().getConnection();
-		PreparedStatement ps = null;
-		ResultSet resultSet = null;
-		List<Transaction> transactionList = new ArrayList<Transaction>();
-
-		try {
-			ps = connection.prepareStatement(PassbookMaintenanceQueryMapper.QUERY_TRANS_DETAILS);
-			ps.setString(1, accountId);
-			resultSet = ps.executeQuery();
-
-			while (resultSet.next()) {
-				Transaction details = new Transaction();
-				details.setId(resultSet.getString(1));
-				details.setTransDate((resultSet.getDate(2)).toLocalDate());
-				details.setAmount(resultSet.getDouble(3));
-				details.setTransFrom(resultSet.getString(4));
-				details.setTransTo(resultSet.getString(5));
-				details.setType(resultSet.getString(6));
-				details.setOption(resultSet.getString(7));
-				details.setChequeId(resultSet.getInt(8));
-				details.setClosingBalance(resultSet.getDouble(9));
-				transactionList.add(details);
-			}
-		} catch (Exception e) {
-            logger.error(e.getMessage());
-			throw new PassbookException(ErrorConstants.TECH_ERROR);
-
-		} finally {
-
-			try {
-
-				resultSet.close();
-				ps.close();
-				connection.close();
-				
-				
-			} catch (Exception e) {
-
-				logger.error(e.getMessage());
-				throw new MyException(ErrorConstants.DB_CONNECTION_ERROR);
-
-			}
+		if (updatePassbook.size() < 1) {
+			System.out.println("no trans");
 		}
 
-		return transactionList;
-		
+		else {
+			for (int index = 0; index < updatePassbook.size(); index++) {
+				System.out.print(updatePassbook.get(index).getId() + "\t");
+				System.out.print(updatePassbook.get(index).getTransDate() + "\t");
+				System.out.print(updatePassbook.get(index).getAmount() + "\t");
+				System.out.print(updatePassbook.get(index).getTransFrom() + "\t");
+				System.out.print(updatePassbook.get(index).getTransTo() + "\t");
+				System.out.print(updatePassbook.get(index).getType() + "\t");
+				System.out.print(updatePassbook.get(index).getOption() + "\t");
+				if (updatePassbook.get(index).getOption().equalsIgnoreCase("cheque")) {
+					System.out.print(updatePassbook.get(index).getChequeId() + "\t");
 
+				} else {
+					System.out.print("-");
+				}
+				System.out.print(updatePassbook.get(index).getClosingBalance() + "\t");
+				System.out.println();
+			}
+		}
+	} catch (MyException | PassbookException e) {
+		System.out.println(e.getMessage());;
 	}
+      
+
+	PassbookMaintenanceService accountSummaryService = new PassbookMaintenanceServiceImpl();
+
 	
-	/*******************************************************************************************************
-	 * - Function Name : updateDate(String accountId) 
-	 * - Input Parameters : String accountId
-	 * - Return Type : boolean 
-	 * - Throws : PassbookException, MyException 
-	 * - Author : Mansi Agarwal
-	 * - Creation Date : 24/09/2019 
-	 * - Description : Updates the date of last transaction that was printed in the passbook
-	 ********************************************************************************************************/
+	System.out.println("Enter accountId:");
+	String accountId1 = obj.nextLine();
 	
 	
-	public boolean updateLastUpdated(String accountId) throws MyException, PassbookException 
+	System.out.println("Enter start date:");
+	String sdate1 = obj.nextLine();
+	
+
+	
+	System.out.println("Enter end date:");
+	String sdate2 = obj.nextLine();
+	
+	LocalDate date1 = LocalDate.parse(sdate1);
+	LocalDate date2 = LocalDate.parse(sdate2);
+	
+	List <Transaction> accountSummary = new ArrayList<Transaction>();
+	accountSummary=accountSummaryService.accountSummary(accountId1,date1,date2);
+
+	if(accountSummary.size()<1)
 	{
-		boolean updated = false;
-		Connection connection = DBConnection.getInstance().getConnection();
-		PreparedStatement ps = null;		
-		int queryResult = 0;
-		
-		try {
-			ps = connection.prepareStatement(PassbookMaintenanceQueryMapper.QUERY_LAST_UPDATED);
-			queryResult= ps.executeUpdate();
-			
-			if(queryResult==0)
-			{
-				logger.error("Updation failed");
-				throw new PassbookException(ErrorConstants.UPDATE_ACCOUNT_ERROR);
-
-			}
-			else {
-				updated = true;
-			}
-				
-		}catch(Exception e) {
-			throw new PassbookException(ErrorConstants.TECH_ERROR);
-		}
-		finally {
-
-			try {
-				ps.close();
-				connection.close();
-			} catch (Exception e) 
-			{
-				logger.error(e.getMessage());
-				throw new MyException(ErrorConstants.DB_CONNECTION_ERROR);
-			}
-		}
-		
-		logger.info("Updation successful");
-		return updated;
-	
+		System.out.println("no trans");
 	}
 
-	
-	
-	/*******************************************************************************************************
-	 * - Function Name : accountSummary(String accountId, Date startDate, Date endDate) 
-	 * - Input Parameters : String accountId, Date startDate, Date endDate
-	 * - Return Type : List 
-	 * - Throws : PassbookException, MyException 
-	 * - Author : Rishav Dev
-	 * - Creation Date : 24/09/2019 
-	 * - Description : Stores the account summary in the list and returns it to service layer
-	 ********************************************************************************************************/
-	
-	
-	
-	
-	
-	@Override
-	public List<Transaction> accountSummary(String accountId, LocalDate startDate, LocalDate endDate)
-			throws PassbookException, MyException {
-
-		Connection connection = DBConnection.getInstance().getConnection();
-		PreparedStatement ps = null;
-		ResultSet resultSet = null;
-		List<Transaction> transactionList = new ArrayList<Transaction>();
-
-		int queryResult = 0;
-
-		try {
-			ps = connection.prepareStatement(PassbookMaintenanceQueryMapper.QUERY_SUMMARY);
-			ps.setString(1, accountId);
-			ps.setDate(2, java.sql.Date.valueOf(startDate));
-			ps.setDate(3, java.sql.Date.valueOf(endDate));
-			resultSet = ps.executeQuery();
-//			queryResult=ps.executeUpdate();
-			while (resultSet.next()) {
-				Transaction details = new Transaction();
-				details.setId(resultSet.getString(1));
-				details.setTransDate((resultSet.getDate(2)).toLocalDate());
-				details.setAmount(resultSet.getDouble(3));
-				details.setTransFrom(resultSet.getString(4));
-				details.setTransTo(resultSet.getString(5));
-				details.setType(resultSet.getString(6));
-				details.setOption(resultSet.getString(7));
-				details.setChequeId(resultSet.getInt(8));
-				details.setClosingBalance(resultSet.getDouble(7));
-				transactionList.add(details);
-			}
-
-			if (queryResult == 0) {
-				logger.error("updation failed ");
-				throw new PassbookException(ErrorConstants.UPDATE_ACCOUNT_ERROR);
+	else
+	{
+		for (int index = 0; index < accountSummary.size(); index++) {
+			System.out.print(accountSummary.get(index).getId() + "\t");
+			System.out.print(accountSummary.get(index).getTransDate() + "\t");
+			System.out.print(accountSummary.get(index).getAmount() + "\t");
+			System.out.print(accountSummary.get(index).getTransFrom() + "\t");
+			System.out.print(accountSummary.get(index).getTransTo() + "\t");
+			System.out.print(accountSummary.get(index).getType() + "\t");
+			System.out.println(accountSummary.get(index).getOption() + "\t");
+			if (accountSummary.get(index).getOption().equalsIgnoreCase("cheque")) {
+				System.out.print(accountSummary.get(index).getChequeId() + "\t");
 
 			} else {
-				logger.info("updation successful:");
-				return transactionList;
+				System.out.print("-");
 			}
-
-		} catch (Exception e) {
-			System.out.println("Here DAO Catch:"+e.getMessage());
-			logger.error(e.getMessage());
-			throw new PassbookException(ErrorConstants.TECH_ERROR);
-
-		} finally {
-
-			try {
-
-				resultSet.close();
-				ps.close();
-				connection.close();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				logger.error(e.getMessage());
-				throw new MyException(ErrorConstants.DB_CONNECTION_ERROR);
-
-			}
+			System.out.println(accountSummary.get(index).getChequeId() + "\t");
+			System.out.print(accountSummary.get(index).getClosingBalance() + "\t");
+			System.out.println();
 		}
-
 	}
-
-	@Override
-	public boolean updateDate(String accountId) throws MyException, PassbookException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	obj.close();}	
+	
 }
+	
--- pecunianew/src/main/java/com/capgemini/pecunia/service/PassbookMaintenanceServiceImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/service/PassbookMaintenanceServiceImpl.java
@@ -9,6 +9,7 @@ import org.apache.log4j.PropertyConfigurator;
 
 import com.capgemini.pecunia.dao.PassbookMaintenanceDAO;
 import com.capgemini.pecunia.dao.PassbookMaintenanceDAOImpl;
+import com.capgemini.pecunia.dto.Account;
 import com.capgemini.pecunia.dto.Transaction;
 import com.capgemini.pecunia.exception.ErrorConstants;
 import com.capgemini.pecunia.exception.MyException;
@@ -35,24 +36,34 @@ public class PassbookMaintenanceServiceImpl implements PassbookMaintenanceServic
 	 ********************************************************************************************************/
 	
 	@Override
-	public List<Transaction> updatePassbook(String accountId) throws MyException, PassbookException{
-
+	public List<Transaction> updatePassbook(String accountId) throws MyException, PassbookException
+	{
+		try {
 		List<Transaction> transactionList = new ArrayList<Transaction>();
 		PassbookMaintenanceDAO pdao = new PassbookMaintenanceDAOImpl();
-		try {
-			transactionList = pdao.updatePassbook(accountId);
-			boolean ans=false;
-			if(transactionList.size()>0) {
-				ans= pdao.updateDate(accountId);
-				if(ans==true)
-				{
-					logger.info("Updation successful");
-				}
+		Account account = new Account();
+		account.setId(accountId);
+		AccountManagementService accountManagementService = new AccountManagementServiceImpl();
+		boolean accountExist = accountManagementService.validateAccountId(account);
+		if(!accountExist)
+		{
+			throw new PassbookException(ErrorConstants.ERROR_VALIDATION);
+		}
+		
+		
+		transactionList = pdao.updatePassbook(accountId);
+		boolean ans=false;
+		if(transactionList.size()>0) {
+			ans= pdao.updateLastUpdated(accountId);
+			if(ans)
+			{
+				logger.info("Updation successful");
 			}
-		} catch (PassbookException | MyException e) {
-			throw new MyException(ErrorConstants.UPDATE_PASSBOOK_ERROR);
 		}
 		return transactionList;
+	} catch (Exception e) {
+		throw new PassbookException(e.getMessage());
+	}
 	}
 
 	/*******************************************************************************************************
@@ -63,21 +74,38 @@ public class PassbookMaintenanceServiceImpl implements PassbookMaintenanceServic
 	 * - Author : Rishav Dev
 	 * - Creation Date : 24/09/2019 
 	 * - Description : Provides the account summary
+	 * @throws PassbookException 
 	 ********************************************************************************************************/
 	
 	
-	
-	
-	@Override
-	public List<Transaction> accountSummary(String accountId, LocalDate startDate, LocalDate endDate) throws MyException {
-		List<Transaction> transactionList = new ArrayList<Transaction>();
-		PassbookMaintenanceDAO pdao = new PassbookMaintenanceDAOImpl();
-		try {
-			transactionList = pdao.accountSummary(accountId, startDate, endDate);
-		} catch (PassbookException | MyException e) {
+	public List<Transaction> accountSummary(String accountId, LocalDate startDate, LocalDate endDate) throws MyException, PassbookException {
 		
-			throw new MyException(ErrorConstants.UPDATE_PASSBOOK_ERROR);
-		}
-		return transactionList;
+		try {
+			List<Transaction> transactionList = new ArrayList<Transaction>();
+			PassbookMaintenanceDAO pdao = new PassbookMaintenanceDAOImpl();
+			Account account = new Account();
+			account.setId(accountId);
+			AccountManagementService accountManagementService = new AccountManagementServiceImpl();
+			boolean accountExist = accountManagementService.validateAccountId(account);
+			if(!accountExist)
+			{
+				throw new PassbookException(ErrorConstants.ERROR_VALIDATION);
+			}
+			
+			
+			transactionList = pdao.updatePassbook(accountId);
+			boolean ans=false;
+			if(transactionList.size()>0) {
+				ans= pdao.updateLastUpdated(accountId);
+				if(ans)
+				{
+					logger.info("Updation successful");
+				}
+			}
+			return transactionList;
+		} catch (Exception e) {
+			throw new PassbookException(e.getMessage());
+
+}
 	}
 }
