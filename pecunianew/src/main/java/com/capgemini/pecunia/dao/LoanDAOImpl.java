7e30dfb2cf4b2646f41bd24021e12346048acff9
Rishav Dev <55138547+DevRishav@users.noreply.github.com>
2019-09-28 13:26:46 +0530

    loan updated

--- pecunianew/src/main/java/com/capgemini/pecunia/dao/LoanDAOImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/dao/LoanDAOImpl.java
@@ -12,65 +12,71 @@ import com.capgemini.pecunia.dto.Loan;
 import com.capgemini.pecunia.exception.ErrorConstants;
 import com.capgemini.pecunia.exception.LoanException;
 import com.capgemini.pecunia.exception.PecuniaException;
-import com.capgemini.pecunia.util.DBConnection;
-
+import com.capgemini.pecunia.util.DBConnection;	
 public class LoanDAOImpl implements LoanDAO {
 
-	Logger logger = Logger.getRootLogger();
-
-	public LoanDAOImpl() {
-		PropertyConfigurator.configure("resources//log4j.properties");
-
+	Logger logger=Logger.getRootLogger();
+	public LoanDAOImpl()
+	{
+	PropertyConfigurator.configure("resources//log4j.properties");
+	
 	}
 
 	/*******************************************************************************************************
-	 * Function Name : fetchAccountId(String accountId) - Input Parameters : String
-	 * accountId * Return Type : String Author : Rishabh Rai - Creation Date :
-	 * 24/09/2019 Description : Fetch account Id from Database Amount , tenure and
-	 * Rate of Interest as parameter and returns emi for the
-	 ********************************************************************************************************/
+	 * -Function Name : fetchAccountId(String accountId) - Input
+	 * -Parameters : String accountId
+	 * -Return Type : String 
+	 * -Author : Rishabh Rai 
+	 * -Creation Date : 24/09/2019
+	 * -Description : Fetch account Id from Database 
+	 * ********************************************************************************************************/
 
-	@Override
+	
 	public String fetchAccountId(String accountId) throws PecuniaException, LoanException {
-
+		
 		Connection connection = DBConnection.getInstance().getConnection();
 		PreparedStatement preparedStatement = null;
 		ResultSet rs = null;
+
 		String Id = null;
 		try {
 			preparedStatement = connection.prepareStatement(LoanQuerryMapper.FETCH_ACCOUNT_ID);
 			preparedStatement.setString(1, accountId);
-			rs = preparedStatement.executeQuery();
-
-			while (rs.next()) {
-				Id = rs.getString(1);
+			rs = preparedStatement.executeQuery();		
+			while(rs.next()) {
+				Id=rs.getString(1);
 			}
-
-		} catch (SQLException e) {
+			
+			return Id;
+		} catch (Exception e) {
 			throw new LoanException(ErrorConstants.FETCH_ERROR);
-		} finally {
+		}
+		finally {
 			try {
 				rs.close();
 				preparedStatement.close();
 				connection.close();
-
+				
 			} catch (SQLException sqlException) {
 				logger.error(sqlException.getMessage());
 				throw new PecuniaException(ErrorConstants.DB_CONNECTION_ERROR);
 			}
 		}
-		return Id;
-
+		
+		
 	}
-
+	
 	/*******************************************************************************************************
-	 * Function Name : addLoanDetails(Loan loan) - Input Parameters : Loan loan
-	 * Return Type : boolean Author : Rishabh Rai - Creation Date : 24/09/2019
-	 * Description : Adding Loan details to database
+	 * -Function Name : addLoanDetails(Loan loan) - Input
+	 * -Parameters : Loan loan
+	 * -Return Type : boolean 
+	 * -Author : Rishabh Rai -
+	 * -Creation Date : 24/09/2019
+	 * -Description : Adding Loan details to database  
 	 ********************************************************************************************************/
 	public boolean addLoanDetails(Loan loan) throws PecuniaException, LoanException {
 		Connection connection = DBConnection.getInstance().getConnection();
-		boolean flag = false;
+		boolean isadditionsuccess = false;
 		PreparedStatement preparedStatement = null;
 
 		int queryResult = 0;
@@ -89,13 +95,15 @@ public class LoanDAOImpl implements LoanDAO {
 
 			if (queryResult == 0) {
 				throw new LoanException(ErrorConstants.LOAN_ADD_ERROR);
-			} else {
-				flag = true;
+			}
+			else
+			{
+				isadditionsuccess = true;
 			}
 
-		} catch (SQLException sqlException) {
+		} catch (Exception sqlException) {
 			logger.error(sqlException.getMessage());
-			throw new PecuniaException(ErrorConstants.DB_CONNECTION_ERROR);
+			throw new PecuniaException(sqlException.getMessage());
 		}
 
 		finally {
@@ -107,8 +115,9 @@ public class LoanDAOImpl implements LoanDAO {
 				throw new PecuniaException(ErrorConstants.DB_CONNECTION_ERROR);
 			}
 		}
-		return flag;
+		return isadditionsuccess;
 
 	}
+
 
 }
--- pecunianew/src/main/java/com/capgemini/pecunia/service/LoanServiceImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/service/LoanServiceImpl.java
@@ -4,15 +4,17 @@ import com.capgemini.pecunia.dao.LoanDAO;
 import com.capgemini.pecunia.dao.LoanDAOImpl;
 import com.capgemini.pecunia.dto.Loan;
 import com.capgemini.pecunia.exception.LoanException;
+import com.capgemini.pecunia.exception.PecuniaException;
 
 public class LoanServiceImpl implements LoanService {
 
 	/*******************************************************************************************************
-	 * - Function Name : calculateEMI(double amount, int tenure, double roi) - Input
-	 * Parameters : double amount, int tenure, double roi * - Return Type : double -
-	 * Author : Rishabh Rai - Creation Date : 24/09/2019 - Description : Takes the
-	 * Amount , tenure and Rate of Interest as parameter and returns emi for the
-	 * loan
+	 * -Function Name : calculateEMI(double amount, int tenure, double roi)
+	 * -Input Parameters : double amount, int tenure, double roi 
+	 * -Return Type : double 
+	 * -Author : Rishabh Rai
+	 * -Creation Date : 24/09/2019
+	 * -Description : Takes the Amount,tenure and Rate of Interest as parameter and returns emi for the loan
 	 ********************************************************************************************************/
 
 	public double calculateEMI(double amount, int tenure, double roi) {
@@ -24,46 +26,51 @@ public class LoanServiceImpl implements LoanService {
 	}
 
 	/*******************************************************************************************************
-	 * - Function Name :createLoanRequest(Loan loan) - Input Parameters : Loan loan
-	 * * - Return Type : boolean - Author : Rishabh Rai - Creation Date : 24/09/2019
-	 * 
-	 * @Throws LoanException - Description : Create entry for loan Request
+	 * -Function Name :createLoanRequest(Loan loan) 
+	 * -Input Parameters : Loan loan
+	 * -Return Type : boolean 
+	 * -Author : Rishabh Rai 
+	 * -Creation Date : 24/09/2019
+	 * -@Throws LoanException  
+	 * -Description : Create entry for loan Request
 	 ********************************************************************************************************/
 
 	public boolean createLoanRequest(Loan loan) throws LoanException {
-		boolean flag = false;
+		boolean isRequestSuccess = false;
 		LoanDAO ld = new LoanDAOImpl();
 		try {
-			flag = ld.addLoanDetails(loan);
+			isRequestSuccess= ld.addLoanDetails(loan);
 
 		} catch (Exception e) {
 			throw new LoanException(e.getMessage());
 		}
-		return flag;
+		return isRequestSuccess;
 
 	}
 
 	/*******************************************************************************************************
-	 * - Function Name :validateCustomerId(String account_ID - Input Parameters :
-	 * String account_ID * - Return Type : boolean - Author : Rishabh Rai - Creation
-	 * Date : 24/09/2019
-	 * 
-	 * @Throws LoanException - Description : Validating account Id
+	 * -Function Name :validateCustomerId(String account_Id) 
+	 * -Input Parameters :String account_ID 
+	 * -Return Type : boolean - Author : Rishabh Rai - Creation
+	 * -Date : 24/09/2019
+	 * -@throws MyException , LoanException 
+	 * -Description : Validating account Id
 	 ********************************************************************************************************/
-	@Override
-	public boolean validateCustomerId(String account_ID) throws LoanException {
+
+	public boolean validateCustomerId(String accountId) throws LoanException{
 		LoanDAO ld = new LoanDAOImpl();
-		boolean flag = false;
+		boolean isValidated = false;
 		try {
-			String ID = ld.fetchAccountId(account_ID);
-			if (ID.equals(account_ID)) {
-				flag = true;
-				return flag;
+			String ID = ld.fetchAccountId(accountId);
+			if (ID.equals(accountId)) {
+				isValidated= true;
+				return isValidated;
 			}
-		} catch (Exception e) {
-			throw new LoanException(e.getMessage());
+		}catch (Exception e) {
+			throw new LoanException("Account Id does not exist");
 		}
-		return flag;
+		
+		return isValidated;
 
 	}
 
