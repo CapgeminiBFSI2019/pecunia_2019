55666a1a471ad6a2b886b3423e2a58be33dc1408
Rohan Patil <rohanpatil365@gmail.com>
2019-09-30 08:14:34 +0530

    changed validator to isValid

--- pecunianew/src/main/java/com/capgemini/pecunia/inputvalidator/TransactionInputValidator.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/inputvalidator/TransactionInputValidator.java
@@ -3,42 +3,42 @@ package com.capgemini.pecunia.inputvalidator;
 public class TransactionInputValidator {
 
 	public boolean validateAccId(String accId) {
-		boolean validator = false;
+		boolean isValid = false;
 		if (accId != null && !accId.isEmpty() && accId.length() == 12) {
 			if (accId.matches("[0-9]{14}")) {
-				validator = true;
+				isValid = true;
 			}
 		}
-		return validator;
+		return isValid;
 	}
 
-	public boolean transactionAmountValidator(double amount) {
-		boolean validator = false;
+	public boolean transactionAmountisValid(double amount) {
+		boolean isValid = false;
 		if (amount > 0) {
-			validator = true;
+			isValid = true;
 		}
-		return validator;
+		return isValid;
 	}
 
-	public boolean chequeNumberValidator(int chequeNum) {
-		boolean validator = false;
+	public boolean chequeNumberisValid(int chequeNum) {
+		boolean isValid = false;
 		if (Integer.toString(chequeNum).length() == 6) {
 			if (Integer.toString(chequeNum).matches("[0-9]{6}")) {
-				validator = true;
+				isValid = true;
 			}
 		}
-		return validator;
+		return isValid;
 	}
 
 	public boolean validateAccountName(String accountName) {
-		boolean validator = false;
+		boolean isValid = false;
 		String[] Nametmp = accountName.split("\\s+");
 		if (accountName != null && !accountName.isEmpty()) {
 			if (Nametmp[0].matches("[A-Za-z]+") && Nametmp[1].matches("[A-Za-z]+")) {
-				validator = true;
+				isValid = true;
 			}
 		}
-		return validator;
+		return isValid;
 	}
 
 }
