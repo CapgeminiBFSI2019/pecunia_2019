bcc71d724d05e5be4a812b82266a2fac2086e65d
vidushi1308 <46959204+vidushi1308@users.noreply.github.com>
2019-10-03 18:16:09 +0530

    completed account servlet

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/AddAccount.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/AddAccount.java
@@ -2,11 +2,21 @@ package com.capgemini.pecunia.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.capgemini.pecunia.dto.Account;
+import com.capgemini.pecunia.dto.Address;
+import com.capgemini.pecunia.dto.Customer;
+import com.capgemini.pecunia.exception.AccountException;
+import com.capgemini.pecunia.exception.PecuniaException;
+import com.capgemini.pecunia.service.AccountManagementService;
+import com.capgemini.pecunia.service.AccountManagementServiceImpl;
 
 /**
  * Servlet implementation class AddAccount
@@ -24,6 +34,7 @@ public class AddAccount extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String dateofbirth = request.getParameter("dateofbirth");
 
 		String contact = request.getParameter("contact");
@@ -39,10 +50,45 @@ public class AddAccount extends HttpServlet {
 
 		String accounttype = request.getParameter("accounttype");
 		String branchid = request.getParameter("branchid");
-//		double accountbalance = request.getParameter(accountbalance);
-//
-//		double accountinterest = request.getParameter(accountinterest);
+		double accountbalance = Double.parseDouble(request.getParameter("accountbalance"));
 
+		double accountinterest = Double.parseDouble(request.getParameter("accountinterest"));
 
-}
+		response.setContentType("html/text");
+		PrintWriter out = response.getWriter();
+
+		Account account = new Account();
+		Address address = new Address();
+		Customer customer = new Customer();
+
+		address.setLine1(addressline1);
+		address.setLine2(addressline2);
+		address.setCity(city);
+		address.setState(state);
+		address.setCountry(country);
+		address.setZipcode(zipcode);
+		account.setAccountType(accounttype);
+		account.setBranchId(branchid);
+		account.setBalance(accountbalance);
+		account.setInterest(accountinterest);
+		customer.setAadhar(aadhar);
+		customer.setContact(contact);
+		customer.setDob(LocalDate.parse(dateofbirth, dateTimeFormatter));
+		customer.setGender(gender);
+		customer.setName(name);
+		customer.setPan(pan);
+
+		AccountManagementService ams = new AccountManagementServiceImpl();
+		try {
+			String created = ams.addAccount(customer, address, account);
+			if (created != null) {
+				request.getRequestDispatcher("addAccount.html").include(request, response);
+				out.println("<h1> Account successfully created </h1>");
+			}
+		} catch (PecuniaException | AccountException e) {
+			out.println("<h1>Failure</h1><br>");
+			request.getRequestDispatcher("addAccount.html").include(request, response);
+		}
+	}
+
 }
