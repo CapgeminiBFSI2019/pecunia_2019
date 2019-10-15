7066438aeed178ca154a8aca9df17fb6f27e7dd8
risrai <55137949+risrai@users.noreply.github.com>
2019-10-15 18:13:18 +0530

    Changed loanrequestservlet file only**

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/LoanRequestServlet.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/LoanRequestServlet.java
@@ -1,7 +1,10 @@
 package com.capgemini.pecunia.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,7 @@ import com.capgemini.pecunia.dto.Loan;
 import com.capgemini.pecunia.exception.LoanException;
 import com.capgemini.pecunia.service.LoanService;
 import com.capgemini.pecunia.service.LoanServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Servlet implementation class LoanRequest1
@@ -21,46 +25,80 @@ public class LoanRequestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException 
+	{
 		HttpSession session = request.getSession(false);
 		if (session == null) {
 		    // Session is not created.
 			response.sendRedirect("session.html");
 		}
-		String AccountId = request.getParameter("Account Id");
-		double amount = Double.parseDouble(request.getParameter("LoanAmount"));
-		int tenure = Integer.parseInt(request.getParameter("Tenure"));
-		double roi = Double.parseDouble(request.getParameter("roi"));
-		int creditScore = Integer.parseInt(request.getParameter("CreditScore"));
-		String loanStatus = request.getParameter("status");
-		String loanType = request.getParameter("LoanType");
-		Loan loan = new Loan();
-		loan.setAccountId(AccountId);
+		StringBuffer jb = new StringBuffer();
+		  String line = null;
+		  try {
+		    BufferedReader reader = request.getReader();
+		    while ((line = reader.readLine()) != null)
+		      jb.append(line);
+		  } catch (Exception e) {  }
+		Map<String,String> myMap = new HashMap<String, String>();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		myMap = objectMapper.readValue(jb.toString(), HashMap.class);
+	    String accountId = myMap.get("accountId");
+	    double amount = Double.parseDouble(myMap.get("amount"));
+	    int tenure=Integer.parseInt(myMap.get("tenure"));
+	    int creditScore=Integer.parseInt(myMap.get("creditScore"));
+	    String status=myMap.get("status");
+	    String type=myMap.get("type");
+	    double roi=Double.parseDouble(myMap.get("roi"));
+	    Loan loan = new Loan();
+	    loan.setAccountId(accountId);
 		loan.setAmount(amount);
 		loan.setTenure(tenure);
 		loan.setCreditScore(creditScore);
-		loan.setLoanStatus(loanStatus);
+		loan.setLoanStatus(status);
 		loan.setRoi(roi);
-		loan.setType(loanType);
+		loan.setType(type);
 		LoanService loanService = new LoanServiceImpl();
 		double emi = LoanServiceImpl.calculateEMI(amount, tenure, roi);
 		loan.setEmi(emi);
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		try {
-			boolean isSuccess = loanService.createLoanRequest(loan);
-			if (isSuccess) {
-				request.getRequestDispatcher("LoanRequest.html").include(request, response);
-				out.println("<script>$('#loan-request-success').toast('show');</script>");
-
-			} else {
-				throw new LoanException("");
-			}
-
-		} catch (LoanException e) {
-			request.getRequestDispatcher("LoanRequest.html").include(request, response);
-			out.println("<script>$('#loan-request-failure').toast('show');</script>");
-		}
-		out.close();
+	    
+	    		
+	    
+//		String AccountId = request.getParameter("Account Id");
+//		double amount = Double.parseDouble(request.getParameter("LoanAmount"));
+//		int tenure = Integer.parseInt(request.getParameter("Tenure"));
+//		double roi = Double.parseDouble(request.getParameter("roi"));
+//		int creditScore = Integer.parseInt(request.getParameter("CreditScore"));
+//		String loanStatus = request.getParameter("status");
+//		String loanType = request.getParameter("LoanType");
+//		Loan loan = new Loan();
+//		loan.setAccountId(AccountId);
+//		loan.setAmount(amount);
+//		loan.setTenure(tenure);
+//		loan.setCreditScore(creditScore);
+//		loan.setLoanStatus(loanStatus);
+//		loan.setRoi(roi);
+//		loan.setType(loanType);
+//		LoanService loanService = new LoanServiceImpl();
+//		double emi = LoanServiceImpl.calculateEMI(amount, tenure, roi);
+//		loan.setEmi(emi);
+//		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		try {
+//			boolean isSuccess = loanService.createLoanRequest(loan);
+//			if (isSuccess) {
+//				request.getRequestDispatcher("LoanRequest.html").include(request, response);
+//				out.println("<script>$('#loan-request-success').toast('show');</script>");
+//
+//			} else {
+//				throw new LoanException("");
+//			}
+//
+//		} catch (LoanException e) {
+//			request.getRequestDispatcher("LoanRequest.html").include(request, response);
+//			out.println("<script>$('#loan-request-failure').toast('show');</script>");
+//		}
+//		out.close();
 	}
 }
