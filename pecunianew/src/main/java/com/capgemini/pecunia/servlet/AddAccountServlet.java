feba91e0631e00194de686fe8d7927182a92b4b5
vidushi1308 <46959204+vidushi1308@users.noreply.github.com>
2019-10-15 17:17:26 +0530

    changed servlet

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/AddAccountServlet.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/AddAccountServlet.java
@@ -23,7 +23,6 @@ import com.capgemini.pecunia.service.AccountManagementServiceImpl;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.sun.javafx.collections.MappingChange.Map;
 
 /**
  * Servlet implementation class AddAccountServlet
