c2263d83346c6adc88b4e4efc22958e4474884ec
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-10-16 11:17:00 +0530

    Update UpdateCustomerNameServlet.java

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/UpdateCustomerNameServlet.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/UpdateCustomerNameServlet.java
@@ -3,14 +3,11 @@ package com.capgemini.pecunia.servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.capgemini.pecunia.dto.Account;
 import com.capgemini.pecunia.dto.Customer;
@@ -18,71 +15,74 @@ import com.capgemini.pecunia.exception.AccountException;
 import com.capgemini.pecunia.exception.PecuniaException;
 import com.capgemini.pecunia.service.AccountManagementService;
 import com.capgemini.pecunia.service.AccountManagementServiceImpl;
-import com.capgemini.pecunia.util.Constants;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class UpdateCustomerNameServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session = request.getSession(false);
-		if (session == null) {
-		    // Session is not created.
-			response.sendRedirect("session.html");
-		}
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+//		HttpSession session = request.getSession(false);
+//		if (session == null) {
+//		    // Session is not created.
+//			response.sendRedirect("session.html");
+//		}
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization, Content-Length, X-Requested-With");
+		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
 		StringBuffer jb = new StringBuffer();
-		  String line = null;
-		  try {
-		    BufferedReader reader = request.getReader();
-		    while ((line = reader.readLine()) != null)
-		      jb.append(line);
-		  } catch (Exception e) {  }
-		Map<String,String> myMap = new HashMap<String, String>();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null)
+				jb.append(line);
+		} catch (Exception e) {
+		}
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		myMap = objectMapper.readValue(jb.toString(), HashMap.class);
-		String accountId = myMap.get("accountId");
+		JsonObject dataResponse = new JsonObject();
 
-		String custName = myMap.get("name");
-		
+		Gson gson = new Gson();
+		JsonElement jelem = gson.fromJson(jb.toString(), JsonElement.class);
+		JsonObject jobj = jelem.getAsJsonObject();
+
+		String accountId = jobj.get("accountNumber").getAsString();
+
+		String custName = jobj.get("name").getAsString();
+
 		Account account = new Account();
 		Customer customer = new Customer();
 		account.setId(accountId);
 		customer.setName(custName);
-		
-		
+
 		AccountManagementService ams = new AccountManagementServiceImpl();
-		
+
 		response.setContentType("application/json");
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		
-		response.setHeader("Access-Control-Allow-Headers" ,"Content-Type, Authorization, Content-Length, X-Requested-With");
-		response.setHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode dataResponse = mapper.createObjectNode();
-		boolean updated=false;
-		PrintWriter out =response.getWriter();
+
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization, Content-Length, X-Requested-With");
+		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+		boolean updated = false;
 
 		try {
 			updated = ams.updateCustomerName(account, customer);
 			if (updated) {
-				((ObjectNode) dataResponse).put("success", updated);
-				((ObjectNode) dataResponse).put("message", "Update Successful");
+				dataResponse.addProperty("success", true);
 //				out.println("<script>$('#update-name-success').toast('show');</script>");
 			}
 		} catch (PecuniaException | AccountException e) {
-			((ObjectNode) dataResponse).put("failure", updated);
-			((ObjectNode) dataResponse).put("message", e.getMessage());
+			dataResponse.addProperty("success", false);
 //			request.getRequestDispatcher("updateName.html").include(request, response);
 //			out.println("<script>$('#update-name-failure').toast('show');</script>");
 
-		}
-		finally {
+		} finally {
 			out.print(dataResponse);
 		}
 
