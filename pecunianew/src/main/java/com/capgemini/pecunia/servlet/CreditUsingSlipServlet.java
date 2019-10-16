e55d9ce5e61c57093eb5202fe6bf9c77bf9bad7f
vidushi1308 <46959204+vidushi1308@users.noreply.github.com>
2019-10-16 15:11:05 +0530

    transaction servlet changed

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/CreditUsingSlipServlet.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/CreditUsingSlipServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.capgemini.pecunia.dto.Transaction;
+import com.capgemini.pecunia.exception.AccountException;
 import com.capgemini.pecunia.exception.PecuniaException;
 import com.capgemini.pecunia.exception.TransactionException;
 import com.capgemini.pecunia.service.TransactionService;
@@ -24,6 +25,7 @@ public class CreditUsingSlipServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		PrintWriter out = response.getWriter();
 		response.setContentType("application/json");
 		response.setHeader("Access-Control-Allow-Origin", "*");
@@ -39,41 +41,76 @@ public class CreditUsingSlipServlet extends HttpServlet {
 		} catch (Exception e) {
 		}
 
+		JsonObject dataResponse = new JsonObject();
+
 		Gson gson = new Gson();
 		JsonElement jelem = gson.fromJson(jb.toString(), JsonElement.class);
 		JsonObject jobj = jelem.getAsJsonObject();
-
-		String accountId = jobj.get("accountNumber").getAsString();
-		double amount = Double.parseDouble(jobj.get("creditSlipAmount").toString());
-
-		HttpSession session = request.getSession(false);
-
-		JsonObject dataResponse = new JsonObject();
-		if (session == null) {
-			// Session is not created.
-			dataResponse.addProperty("success", false);
-			dataResponse.addProperty("message", "Session has expired");
-
-			out.print(dataResponse);
-			return;
-		}
-
 		Transaction creditSlip = new Transaction();
+		
+		String accountId = jobj.get("accountId").getAsString();
+		double amount = Double.parseDouble(jobj.get("amount").toString());
+		
 		creditSlip.setAccountId(accountId);
 		creditSlip.setAmount(amount);
+		
 		TransactionService trans = new TransactionServiceImpl();
+		
+		response.setContentType("application/json");
+		response.setHeader("Access-Control-Allow-Origin", "*");
 
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization, Content-Length, X-Requested-With");
+		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+//		Object session = null;
+//		if (session == null) {
+//			// Session is not created.
+//			dataResponse.addProperty("success", false);
+//			dataResponse.addProperty("message", "Session has expired");
+//
+//			out.print(dataResponse);
+//			return;
+//		}
+
+//		try {
+//			
+//			int transId = trans.creditUsingSlip(creditSlip);
+//			dataResponse.addProperty("success", true);
+//			dataResponse.addProperty("transaction_id", transId);
+//			dataResponse.addProperty("message", "Amount has been credited. Transaction id is " + transId);
+//
+//		} catch (TransactionException | PecuniaException e) {
+//			dataResponse.addProperty("success", false);
+//			dataResponse.addProperty("message", e.getMessage());
+//		} finally {
+//			out.print(dataResponse);
+//		}
+//	}
+//}
 		try {
 			int transId = trans.creditUsingSlip(creditSlip);
-			dataResponse.addProperty("success", true);
-			dataResponse.addProperty("transaction_id", transId);
-			dataResponse.addProperty("message", "Amount has been credited. Transaction id is " + transId);
-
-		} catch (TransactionException | PecuniaException e) {
+			
+				dataResponse.addProperty("success", true);
+				dataResponse.addProperty("Account Id", transId);
+				dataResponse.addProperty("message", "Amount credited.Trans Id is \t" + transId);
+//				request.getRequestDispatcher("addAccount.html").include(request, response);
+//				out.println("<script>");
+//				out.println("$('#success-toast-body').html('Account created successfully. Account id is \t" + created + "');");
+//				out.println("$('#add-account-success').toast('show');");
+//				out.println("</script>");
+			
+		} catch (PecuniaException | TransactionException e) {
 			dataResponse.addProperty("success", false);
 			dataResponse.addProperty("message", e.getMessage());
+
+		
+//			request.getRequestDispatcher("addAccount.html").include(request, response);
+//			out.println("<script>$('#add-account-failure').toast('show');</script>");
+
 		} finally {
 			out.print(dataResponse);
 		}
+
 	}
+
 }
