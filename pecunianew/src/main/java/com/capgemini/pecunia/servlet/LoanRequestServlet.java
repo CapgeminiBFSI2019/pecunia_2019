6ad19b51a26a9834512a90b26faac022e512f7de
risrai <55137949+risrai@users.noreply.github.com>
2019-10-16 15:19:07 +0530

    only loan request servlet file

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/LoanRequestServlet.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/LoanRequestServlet.java
@@ -3,24 +3,19 @@ package com.capgemini.pecunia.servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.capgemini.pecunia.dto.Loan;
 import com.capgemini.pecunia.exception.LoanException;
 import com.capgemini.pecunia.service.LoanService;
 import com.capgemini.pecunia.service.LoanServiceImpl;
-
-import com.capgemini.pecunia.util.Constants;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 /**
  * Servlet implementation class LoanRequest1
@@ -31,34 +26,36 @@ public class LoanRequestServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 	
-		HttpSession session = request.getSession(false);
-		if (session == null) {
-		    // Session is not created.
-			response.sendRedirect("session.html");
-		}
-		
-		
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization, Content-Length, X-Requested-With");
+		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
 		StringBuffer jb = new StringBuffer();
-		  String line = null;
-		  try {
-			    BufferedReader reader = request.getReader();
-			    while ((line = reader.readLine()) != null)
-			      jb.append(line);
-			  } catch (Exception e) {  }
-			HashMap<String,String> myMap = new HashMap<String, String>();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null)
+				jb.append(line);
+		} catch (Exception e) {
+		}
 
-			ObjectMapper objectMapper = new ObjectMapper();
-			
-			myMap = objectMapper.readValue(jb.toString(), HashMap.class);
+		JsonObject dataResponse = new JsonObject();
+
+		Gson gson = new Gson();
+		JsonElement jelem = gson.fromJson(jb.toString(), JsonElement.class);
+		JsonObject jobj = jelem.getAsJsonObject();
+		
 			 Loan loan = new Loan();
 			
-	    String accountId = myMap.get("accountId");
-	    double amount = Double.parseDouble(myMap.get("amount"));
-	    String type=myMap.get("type");
-	    double roi=Double.parseDouble(myMap.get("roi"));
-	    int creditScore=Integer.parseInt(myMap.get("creditScore"));
-	    int tenure=Integer.parseInt(myMap.get("tenure"));
-	    String status=myMap.get("status");
+	    String accountId = jobj.get("accountId").getAsString();
+	    double amount = Double.parseDouble(jobj.get("amount").getAsString());
+	    String type=jobj.get("type").getAsString();
+	    double roi=Double.parseDouble(jobj.get("roi").getAsString());
+	    int creditScore=Integer.parseInt(jobj.get("creditScore").getAsString());
+	    int tenure=Integer.parseInt(jobj.get("tenure").getAsString());
+	    String status=jobj.get("status").getAsString();
 	    
 	   
 	    
@@ -79,15 +76,13 @@ public class LoanRequestServlet extends HttpServlet {
 		
 		response.setHeader("Access-Control-Allow-Headers" ,"Content-Type, Authorization, Content-Length, X-Requested-With");
 		response.setHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode dataResponse = mapper.createObjectNode();
-		boolean result=false;
-		PrintWriter out =response.getWriter();
+		
+		
+	
 		try {
 			boolean isSuccess = loanService.createLoanRequest(loan);
 			if (isSuccess) {
-				 ((ObjectNode) dataResponse).put("success", result);
-					((ObjectNode) dataResponse).put("message",Constants.LOAN_REQUEST_SUCCESSFUL);
+				dataResponse.addProperty("success", true);
 
 
 			} 
@@ -99,8 +94,13 @@ public class LoanRequestServlet extends HttpServlet {
 		} 
 		catch (LoanException e)
 		{
+			dataResponse.addProperty("success", false);
+			dataResponse.addProperty("message", e.getMessage());
 			
 		}
+		finally {
+			out.print(dataResponse);
 		}
 	}
+}
 
