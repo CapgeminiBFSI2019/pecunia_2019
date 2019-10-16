0957819f958895271217832331987f4a5e1c344a
Rohan Patil <rohanpatil365@gmail.com>
2019-10-16 08:46:18 +0530

    commit loginServlet/pom

--- pecunianew/src/main/java/com/capgemini/pecunia/servlet/LogoutServlet.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/servlet/LogoutServlet.java
@@ -1,33 +1,31 @@
 package com.capgemini.pecunia.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setContentType("text/html");  
-        PrintWriter out=response.getWriter();  
-          
-          
-        request.getRequestDispatcher("login.html").include(request, response);  
-          
-        HttpSession session = request.getSession();
-        session.invalidate();
-        response.sendRedirect("login.html");
-        out.println("<h4 class='text-success'>You have been successfully logged out. please login again to continue.</h4>"); 
+	
 		
-	}
-
+		
+	}	
+		
+	
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			
+			resp.setContentType("application/json");
+			resp.addHeader("Access-Control-Allow-Origin", "*");
+			resp.getWriter().write("{}");
+		}		
+		
 }
