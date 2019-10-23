c03bea54e7a7693bab5cb25f3328cba96bd749ba
Rohan Patil <rohanpatil365@gmail.com>
2019-10-23 15:44:29 +0530

    Added Config file

--- pecunianew/src/main/java/com/capgemini/pecunia/hibernate/dao/TransactionDAOImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/hibernate/dao/TransactionDAOImpl.java
@@ -1,16 +1,39 @@
 package com.capgemini.pecunia.hibernate.dao;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 import com.capgemini.pecunia.dto.Account;
 import com.capgemini.pecunia.dto.Cheque;
 import com.capgemini.pecunia.dto.Transaction;
+import com.capgemini.pecunia.entity.TransactionEntity;
 import com.capgemini.pecunia.exception.PecuniaException;
 import com.capgemini.pecunia.exception.TransactionException;
+import com.capgemini.pecunia.util.HibernateUtil;
 
 public class TransactionDAOImpl implements TransactionDAO {
 
 	@Override
 	public double getBalance(Account account) throws PecuniaException, TransactionException {
-		// TODO Auto-generated method stub
+		org.hibernate.Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            Query query = session.createQuery("from TransactionEntity where id=:transactionId");
+            query.setParameter("transactionId", 200);
+            query.setMaxResults(1);
+            TransactionEntity mytransaction = (TransactionEntity) query.uniqueResult();
+            System.out.println(mytransaction.getAmount());
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+		
 		return 0;
 	}
 
