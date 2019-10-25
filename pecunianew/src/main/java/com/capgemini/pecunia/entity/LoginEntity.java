ba2632908d0ddd31abb985367be1c11be88f9916
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-10-25 09:24:40 +0530

    Login functionality

--- pecunianew/src/main/java/com/capgemini/pecunia/entity/LoginEntity.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/entity/LoginEntity.java
@@ -7,51 +7,48 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
-	@Entity
-	@NamedQueries({
+@Entity
+@NamedQueries({
 		@NamedQuery(name = "LoginEntity.getsecret_keyByusername", query = "FROM LoginEntity where Username =:email") })
 
-	@Table(name = "login")
-	public class LoginEntity {
+@Table(name = "login")
+public class LoginEntity {
 
-		@Id
-		@Column(name = "email")
-		private String username;
-		@Column(name = "password")
-		private String password;
-		
-	
+	@Id
+	@Column(name = "email")
+	private String username;
+	@Column(name = "password")
+	private String password;
 
-public LoginEntity() {
-	
+	@Column(name = "secret_key")
+	private String secretKey;
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
+
+	public LoginEntity() {
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
-
-
-
-public String getUsername() {
-	return username;
-}
-
-
-
-public void setUsername(String username) {
-	this.username = username;
-}
-
-
-
-public String getPassword() {
-	return password;
-}
-
-
-
-public void setPassword(String password) {
-	this.password = password;
-}
-
-
-}
-
-
--- pecunianew/src/main/java/com/capgemini/pecunia/hibernate/dao/LoginDAOImpl.java
+++ pecunianew/src/main/java/com/capgemini/pecunia/hibernate/dao/LoginDAOImpl.java
@@ -8,119 +8,6 @@ import com.capgemini.pecunia.entity.LoginEntity;
 import com.capgemini.pecunia.exception.ErrorConstants;
 import com.capgemini.pecunia.exception.LoginException;
 import com.capgemini.pecunia.exception.PecuniaException;
-
-import com.capgemini.pecunia.dao.LoginQueryMapper;
-import com.capgemini.pecunia.dto.Login;
-import com.capgemini.pecunia.entity.AccountEntity;
-import com.capgemini.pecunia.entity.LoginEntity;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.hibernate.Session;package com.capgemini.pecunia.hibernate.dao;
-
-import com.capgemini.pecunia.dao.LoginQueryMapper;
-import com.capgemini.pecunia.dto.Login;
-import com.capgemini.pecunia.entity.AccountEntity;
-import com.capgemini.pecunia.entity.LoginEntity;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-
-import com.capgemini.pecunia.exception.ErrorConstants;
-import com.capgemini.pecunia.exception.LoginException;
-import com.capgemini.pecunia.exception.PecuniaException;
-import com.capgemini.pecunia.util.DBConnection;
-import com.capgemini.pecunia.util.HibernateUtil;
-
-public class LoginDAOImpl implements LoginDAO {
-
-	@Override
-	public String validateEmail(Login login) throws PecuniaException, LoginException {
-		String secretKey = null;
-		org.hibernate.Transaction transaction = null;
-		try {
-			Session session = HibernateUtil.getSessionFactory().openSession();
-			transaction = session.beginTransaction();
-			@SuppressWarnings("rawtypes")
-			Query query = session.createNamedQuery("LoginEntity.getsecret_keyByusername");
-			query.setParameter("secretkey", secretKey);
-			query.setMaxResults(1);
-			LoginEntity loginEntity = (LoginEntity) query.uniqueResult();
-			if (loginEntity != null) {
-				secretKey = loginEntity.getSecretKey();
-				System.out.println("jordi bhai");
-
-			} else {
-				throw new PecuniaException(ErrorConstants.NO_SUCH_ACCOUNT);
-			}
-
-	
-		
-		transaction.commit();
-		session.close();
-	}
-	catch (Exception e) {
-		throw new PecuniaException(e.getMessage());
-	}
-		return secretKey;
-
-	}
-
-	@Override
-	public String fetchPassword(Login login) throws PecuniaException, LoginException {
-		String password = null;
-
-		org.hibernate.Transaction transaction = null;
-		try {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		transaction = session.beginTransaction();
-		@SuppressWarnings("rawtypes")
-		Query query = session.createNamedQuery("LoginEntity.getpasswordByusername");
-		query.setParameter("password", password);
-		query.setMaxResults(1);
-		LoginEntity loginEntity = (LoginEntity) query.uniqueResult();
-		if (loginEntity != null) {
-			password = loginEntity.getPassword();
-		}else {
-			throw new PecuniaException(ErrorConstants.LOGIN_ERROR);
-		}
-
-		transaction.commit();
-		session.close();
-		}
-		catch (Exception e) {
-			throw new PecuniaException(e.getMessage());
-		}
-		return password;
-
-	}
-}
-
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-
-import com.capgemini.pecunia.exception.ErrorConstants;
-import com.capgemini.pecunia.exception.LoginException;
-import com.capgemini.pecunia.exception.PecuniaException;
-import com.capgemini.pecunia.util.DBConnection;
-
 import com.capgemini.pecunia.util.HibernateUtil;
 
 public class LoginDAOImpl implements LoginDAO {
@@ -152,7 +39,6 @@ public class LoginDAOImpl implements LoginDAO {
 			throw new PecuniaException(e.getMessage());
 		}
 
-
 		String username = login.getUsername();
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		transaction = session.beginTransaction();
@@ -168,7 +54,6 @@ public class LoginDAOImpl implements LoginDAO {
 		}
 		transaction.commit();
 		session.close();
-
 
 		return secretKey;
 
@@ -203,30 +88,5 @@ public class LoginDAOImpl implements LoginDAO {
 
 	}
 
-		org.hibernate.Transaction transaction = null;
-
-		String password = null;
-
-		String username = login.getPassword();
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		transaction = session.beginTransaction();
-		@SuppressWarnings("rawtypes")
-		Query query = session.createNamedQuery("LoginEntity.getpasswordByusername");
-		query.setParameter("password", password);
-		query.setMaxResults(1);
-		LoginEntity loginEntity = (LoginEntity) query.uniqueResult();
-		if (loginEntity != null) {
-			password = loginEntity.getPassword();
-			}
-			
-			transaction.commit();
-			session.close();
-			
-
-	
-	return password;
-
-	
 }
 
-}
